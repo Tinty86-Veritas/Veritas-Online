bd0e8e46c0544b374df1540651a604cf73b4b809
Tinty <vladimirhomutov55917@gmail.com>
2025-03-19 01:09:14 +0300

    intermediate commit

--- app/src/main/java/com/veritas/veritas/MainActivity.java
+++ app/src/main/java/com/veritas/veritas/MainActivity.java
@@ -1,17 +1,29 @@
 package com.veritas.veritas;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatButton presets_option_button;
+    private AppCompatButton direct_ai_using_option_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        presets_option_button = findViewById(R.id.presets_option_button);
+        direct_ai_using_option_button = findViewById(R.id.direct_ai_using_option_button);
+
+        presets_option_button.setOnClickListener(view -> {
+
+        });
     }
 }
