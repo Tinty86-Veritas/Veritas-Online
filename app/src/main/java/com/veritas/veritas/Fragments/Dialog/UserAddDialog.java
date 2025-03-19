4f3fe001f94ad0f0112e42a815189e93d5b04eb6
Tinty <vladimirhomutov55917@gmail.com>
2025-03-19 14:12:57 +0300

    Add dialog to add user

---
+++ app/src/main/java/com/veritas/veritas/Fragments/Dialog/UserAddDialog.java
@@ -0,0 +1,58 @@
+package com.veritas.veritas.Fragments.Dialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import com.veritas.veritas.R;
+
+public class UserAddDialog extends DialogFragment {
+
+    private EditText editTextName;
+
+    private RadioGroup sex_radio_group;
+
+    private String selected_sex;
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.add_user_dialog, null);
+
+        builder.setView(view)
+                .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        editTextName = view.findViewById(R.id.edit_text_name);
+                        sex_radio_group = view.findViewById(R.id.sex_radio_group);
+
+                        int selected_sex_id = sex_radio_group.getCheckedRadioButtonId();
+
+                        if (selected_sex_id == R.id.radio_button_male) {
+                            selected_sex = getString(R.string.male);
+                        } else {
+                            selected_sex = getString(R.string.female);
+                        }
+
+                        Log.i("UserAddDialog", "name: " + editTextName.getText() + "\nsex: " + selected_sex);
+//                        UsersDB usersDB = new UsersDB(requireContext());
+//                        usersDB.insert()
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        UserAddDialog.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
--- app/src/main/java/com/veritas/veritas/Fragments/SettingsFragment.java
+++ app/src/main/java/com/veritas/veritas/Fragments/SettingsFragment.java
@@ -4,15 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.veritas.veritas.Fragments.Dialog.UserAddDialog;
 import com.veritas.veritas.R;
 
+import java.util.ArrayList;
+
 public class SettingsFragment extends Fragment {
+
+//    ArrayList<String> users_list = new ArrayList<>();
+
+    ListView users_list_view;
+    Button user_add_button;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings_fragment, container, false);
+
+        users_list_view = view.findViewById(R.id.users_list);
+        user_add_button = view.findViewById(R.id.user_add_button);
+
+        user_add_button.setOnClickListener(view1 -> {
+            FragmentManager manager = getParentFragmentManager();
+            UserAddDialog myDialogFragment = new UserAddDialog();
+            myDialogFragment.show(manager, "myDialog");
+        });
 
         return view;
     }
